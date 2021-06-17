a7d18ac1c23f046ea532898154ebea12d80aa893
sevader14 <vsevolod.deriushkin@gmail.com>
2021-06-17 14:16:50 +0200

    adds unit and request tests for comments

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  describe 'Database table' do
+    it { is_expected.to have_db_column :body }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :body }
+  end
+
+  describe 'Associations' do
+    it { is_expected.to belong_to :article }
+    it { is_expected.to belong_to :user }
+  end
+
+  describe 'Factories' do
+    it 'is expected to have valid factory' do
+      expect(create(:comment)).to be_valid
+    end    
+  end
+end
---
+++ spec/requests/api/comment/user_can_create_a_comment_spec.rb
@@ -0,0 +1,57 @@
+RSpec.describe 'POST /api/articles/:id/comments', type: :request do
+  let(:articles) { create(:article) }
+  let(:user) { create(:user) }
+  let(:auth_headers) { user.create_new_auth_token }
+
+  describe 'successfully as a user' do
+    before do
+      post "api/articles/#{article.id}/comments", params: {
+        body: 'User\'s comment body'
+      }, headers: auth_headers
+    end
+
+    it 'is expected to respond with status 201' do
+      expect(response).to have_http_status 201
+    end
+
+    it 'is expected to respond with success message' do
+      expect(response['message']).to eq 'Your comment has been created'
+    end
+
+    it 'is expected to create a comment' do
+      expect(Comment.last['body']).to eq 'User\'s comment body'
+    end
+  end
+
+  describe 'unsuccessfully with no auth headers' do
+    before do
+      post "api/articles/#{article.id}/comments", params: {
+        body: 'Visitors\'s comment body'
+      }
+    end
+
+    it 'is expected to respond with status 401' do
+      expect(response).to have_http_status 401
+    end
+
+    it 'is expected to respond with success message' do
+      expect(response['error_message']).to eq 'Log in to create a comment'
+    end
+  end
+
+  describe 'unsuccessfully with empty body' do
+    before do
+      post "api/articles/#{article.id}/comments", params: {
+        body: ''
+      }
+    end
+
+    it 'is expected to respond with status 422' do
+      expect(response).to have_http_status 422
+    end
+
+    it 'is expected to respond with success message' do
+      expect(response['error_message']).to eq 'Comment can not be empty'
+    end
+  end
+end
